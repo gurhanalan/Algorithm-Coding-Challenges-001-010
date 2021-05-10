f242aa9bbf3723dc525ca7851734ca67e9e95b28
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2021-05-10 19:06:14 +0300

    Adding a new challenge 6

--- script.js
+++ script.js
@@ -138,3 +138,22 @@ function minMax2(arrNums) {
 }
 
 console.log(minMax2([1, 2, 3, 4, 5]));
+
+// 6. Friend or Foe?
+/* 
+Make a program that filters a list of strings and returns a list with only your friends name in it.
+
+If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
+
+Ex: Input = ["Ryan", "Kieran", "Jason", "Yous"], Output = ["Ryan", "Yous"]
+
+i.e.
+
+friend ["Ryan", "Kieran", "Mark"] `shouldBe` ["Ryan", "Mark"]
+Note: keep the original order of the names in the output. */
+
+function friend(friends) {
+    return friends.filter((el) => /^[a-z]{4}$/i.test(el));
+}
+console.log(friend(["Ryan", "Kieran", "Mark"]));
+console.log(/^[a-z]{4}$/i.test("1yan"));
