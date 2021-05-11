c27add7f9d2640760beaa48c6667e6e185258f19
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2021-05-11 20:34:31 +0300

    Adding a new challenge 8

--- script.js
+++ script.js
@@ -186,3 +186,21 @@ function printerError2(s) {
 }
 
 console.log(printerError2("aaaxbbbbyyhwawiwjjjwwm"));
+
+// 8. Mumbling
+/* This time no story, no theory. The examples below show you how to write function accum:
+
+Examples:
+
+accum("abcd") -> "A-Bb-Ccc-Dddd"
+accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+accum("cwAt") -> "C-Ww-Aaa-Tttt"
+The parameter of accum is a string which includes only letters from a..z and A..Z. */
+
+function accum(s) {
+    return s
+        .split("")
+        .map((el, i) => el.toUpperCase() + el.toLowerCase().repeat(i))
+        .join("-");
+}
+console.log(accum("RqaEzty"));
